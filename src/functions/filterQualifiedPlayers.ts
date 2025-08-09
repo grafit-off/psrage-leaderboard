7316fdd8d729a54afeb237e329f0aa38b1896874
Mykola Chornyi <graf.off.it@gmail.com>
2025-08-09 18:06:13 +0300

    chore: min matches to 5

--- src/functions/filterQualifiedPlayers.ts
+++ src/functions/filterQualifiedPlayers.ts
@@ -1,7 +1,7 @@
 import { Player } from '../models/Player';
 
 const filterQualifiedPlayers = (players: Player[]): Player[] => {
-  return players.filter(player => player.stats.Matches > 3);
+  return players.filter(player => player.stats.Matches > 4);
 };
 
 export default filterQualifiedPlayers;
